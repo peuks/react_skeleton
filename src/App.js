84bcd48beae4cf61df6bb10cd17a9d8df5d7572a
peuks <vanmakdavid.lwg@gmail.com>
2021-10-09 18:26:08 +0200

    UPDATE: Advanced routing system management

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import ROUTES, { RenderRoutes } from "./routes";
 
 const App = () => {
   return (
-    <>
-      {/*Put some routing here  */}
-      {/*Put some routing here  */}
-    </>
+    <div style={{ display: "flex", height: "100vh", alignItems: "stretch" }}>
+      <div style={{ flex: 0.3, backgroundColor: "#f2f2f2" }}>route menu</div>
+      <div>
+        <RenderRoutes routes={ROUTES} />
+      </div>
+    </div>
   );
 };
 
